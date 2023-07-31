c0921c336c8346e4e5ee9a4006e4140dfa60c1b1
DMouayad <muayad.perun@outlook,com>
2023-07-31 18:44:18 +0300

    new UI for `TrackHistoryCard`

--- lib/presentation/pages/listening_history_page/tracks_listening_histories_list_view.dart
+++ lib/presentation/pages/listening_history_page/tracks_listening_histories_list_view.dart
@@ -1,26 +1,22 @@
-import 'package:dune/domain/audio/base_models/base_album.dart';
-import 'package:dune/domain/audio/base_models/base_artist.dart';
 import 'package:flutter/material.dart';
 
 //
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 //
+import 'package:dune/presentation/providers/shared_providers.dart';
 import 'package:dune/domain/audio/base_models/base_track.dart';
 import 'package:dune/domain/audio/base_models/base_track_listening_history.dart';
 import 'package:dune/presentation/controllers/selection_controller.dart';
-import 'package:dune/presentation/custom_widgets/track_card.dart';
-import 'package:dune/presentation/custom_widgets/track_card_pop_up_menu.dart';
-import 'package:dune/presentation/custom_widgets/track_card_wrapper.dart';
-import 'package:dune/presentation/providers/state_controllers.dart';
 import 'package:dune/support/extensions/context_extensions.dart';
-import 'package:dune/support/extensions/extensions.dart';
+
+import 'tracks_tab_components/track_history_card.dart';
 
 final trackListeningHistoryCardsSelectionControllerProvider =
-    StateNotifierProvider<SelectionController<BaseTrack>,
-        SelectionState<BaseTrack>>(
-  (ref) => SelectionController<BaseTrack>(SelectionState.initialState()),
+    TracksSelectionControllerProvider(
+  (ref) => SelectionController<BaseTrack>(
+    SelectionState<BaseTrack>.initial(itemToString: (track) => track.title),
+  ),
 );
 
 class TracksListeningHistoriesListView extends ConsumerWidget {
@@ -39,153 +35,18 @@ class TracksListeningHistoriesListView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final tracksRecords = tracksListeningHistories.reversed.toList();
-    return AnimationLimiter(
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemExtent: 72,
-        padding: listPadding ??
-            (compact
-                ? const EdgeInsets.only(top: 22)
-                : const EdgeInsets.all(8)),
-        itemCount: tracksRecords.length,
-        itemBuilder: (context, index) {
-          return AnimationConfiguration.staggeredList(
-            position: index,
-            duration: const Duration(milliseconds: 375),
-            child: SlideAnimation(
-              horizontalOffset: 50.0,
-              child: FadeInAnimation(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 4.0),
-                  child: TrackHistoryCard(
-                    trackListeningHistory: tracksRecords.elementAt(index),
-                    color: context.colorScheme.background,
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+    return ListView.builder(
+      shrinkWrap: true,
+      itemExtent: 60,
+      padding: listPadding ??
+          (compact ? const EdgeInsets.only(top: 22) : const EdgeInsets.all(8)),
+      itemCount: tracksListeningHistories.length,
+      itemBuilder: (context, index) {
+        return TrackHistoryCard(
+          trackListeningHistory: tracksListeningHistories.elementAt(index),
+          color: context.colorScheme.secondary.withOpacity(.04),
+        );
+      },
     );
-  }
-}
-
-class TrackHistoryCard extends ConsumerWidget {
-  const TrackHistoryCard({
-    super.key,
-    required this.trackListeningHistory,
-    required this.color,
-  });
-
-  final BaseTrackListeningHistory trackListeningHistory;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final BaseTrack? track = trackListeningHistory.track;
-    if (track == null) return const Text("Track was not found");
-
-    final textTheme = context.textTheme.bodyMedium?.copyWith(
-      color: context.colorScheme.secondary,
-    );
-    final popupMenu = TrackCardPopupMenu(
-      track,
-      additionalItems: [
-        (
-          text: 'show Track listening history',
-          icon: Icons.history,
-          onPressed: () {},
-        ),
-      ],
-      onSelectTrack: () => _onSelected(ref, track),
-    );
-    return TrackCardWrapper(
-      playOnTap: false,
-      onPlayTrack: () => _playTrack(track, ref),
-      track: track,
-      selectionState:
-          ref.watch(trackListeningHistoryCardsSelectionControllerProvider),
-      onSelected: () => _onSelected(ref, track),
-      cardColor: color,
-      popupMenu: popupMenu,
-      child: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: SizedBox(
-              height: 60,
-              child: TrackCardMainContent(
-                track: track,
-                showAlbumName: false,
-                showArtistsNames: false,
-                showDuration: false,
-                alwaysCenterTitle: true,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                onThumbnailPressed: () => _playTrack(track, ref),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 100,
-            bottom: 0,
-            right: 8,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "complete listens: ${trackListeningHistory.completedListensCount}",
-                  style: textTheme,
-                ),
-                // Spacer(),
-                Text(
-                  "uncompleted listens total: "
-                  "${trackListeningHistory.uncompletedListensTotalDuration?.formatInHhMmSs ?? 0}",
-                  style: textTheme,
-                ),
-                Tooltip(
-                  message: "More",
-                  child: GestureDetector(
-                    onTapDown: (tapDetails) {
-                      final isSelected = ref
-                          .watch(
-                              trackListeningHistoryCardsSelectionControllerProvider)
-                          .selectedValues
-                          .containsKey(track.id);
-                      showTrackCardPopupMenu(
-                        context,
-                        isSelected,
-                        Offset(
-                          tapDetails.globalPosition.dx,
-                          tapDetails.localPosition.dy,
-                        ),
-                        popupMenu,
-                      );
-                    },
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.more_horiz_rounded, size: 20),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _onSelected(WidgetRef ref, BaseTrack<BaseAlbum, BaseArtist> track) {
-    ref
-        .read(trackListeningHistoryCardsSelectionControllerProvider.notifier)
-        .toggleSelectionForItem(track.id, track);
-  }
-
-  void _playTrack(BaseTrack track, WidgetRef ref) {
-    ref.read(playbackControllerProvider.notifier).player.playSingleTrack(track);
   }
 }
---
+++ lib/presentation/pages/listening_history_page/tracks_tab_components/track_history_card.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//
+import 'package:dune/domain/audio/base_models/base_track.dart';
+import 'package:dune/domain/audio/base_models/base_track_listening_history.dart';
+import 'package:dune/presentation/custom_widgets/placeholders.dart';
+import 'package:dune/presentation/custom_widgets/thumbnail_widget.dart';
+import 'package:dune/presentation/custom_widgets/track_card_pop_up_menu.dart';
+import 'package:dune/presentation/custom_widgets/track_card_wrapper.dart';
+import 'package:dune/presentation/pages/listening_history_page/tracks_listening_histories_list_view.dart';
+import 'package:dune/presentation/providers/state_controllers.dart';
+import 'package:dune/support/extensions/context_extensions.dart';
+import 'package:dune/support/extensions/extensions.dart';
+
+class TrackHistoryCard extends ConsumerWidget {
+  const TrackHistoryCard({
+    super.key,
+    required this.trackListeningHistory,
+    required this.color,
+  });
+
+  final BaseTrackListeningHistory trackListeningHistory;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final BaseTrack? track = trackListeningHistory.track;
+    if (track == null) return const Text("Track was not found");
+
+    final popupMenu = TrackCardPopupMenu(
+      track,
+      onSelectTrack: () => _onSelected(ref, track),
+    );
+    final selectionState =
+        ref.watch(trackListeningHistoryCardsSelectionControllerProvider);
+
+    return TrackCardWrapper(
+      playOnTap: false,
+      onPlayTrack: () => _playTrack(track, ref),
+      track: track,
+      selectionState: selectionState,
+      onSelected: () => _onSelected(ref, track),
+      cardColor: color,
+      popupMenu: popupMenu,
+      child: Flex(
+        direction: Axis.horizontal,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: _TrackInfoPart(
+              track: track,
+              selectionEnabled: selectionState.selectionEnabled,
+              playTrack: () => _playTrack(track, ref),
+            ),
+          ),
+          Expanded(
+            child: _ListeningHistoryInfoPart(trackListeningHistory),
+          ),
+          Expanded(
+            flex: 0,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.history, size: 18),
+            ),
+          ),
+          _MoreButton(
+            selected: selectionState.selectedValues.containsKey(track.id),
+            popupMenu: popupMenu,
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+    );
+  }
+
+  void _onSelected(WidgetRef ref, BaseTrack track) {
+    ref
+        .read(trackListeningHistoryCardsSelectionControllerProvider.notifier)
+        .toggleSelectionForItem(track.id, track);
+  }
+
+  void _playTrack(BaseTrack track, WidgetRef ref) {
+    ref.read(playbackControllerProvider.notifier).player.playSingleTrack(track);
+  }
+}
+
+class _TrackInfoPart extends StatelessWidget {
+  const _TrackInfoPart({
+    required this.track,
+    required this.selectionEnabled,
+    required this.playTrack,
+  });
+
+  final BaseTrack track;
+  final bool selectionEnabled;
+  final void Function() playTrack;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: context.platformBorderRadius,
+      onTap: selectionEnabled ? null : () => playTrack(),
+      child: Tooltip(
+        message: selectionEnabled ? 'Tap to toggle selection' : 'Tap to play',
+        waitDuration: const Duration(milliseconds: 1000),
+        child: Row(
+          children: [
+            const SizedBox(width: 10),
+            Expanded(
+              flex: 0,
+              child: ThumbnailWidget(
+                thumbnailsSet: track.thumbnails,
+                dimension: context.isDesktop ? 56 : 48,
+                cacheNetworkImage: true,
+                placeholder: const TrackCoverPlaceholder(),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 6,
+                  // vertical: 3,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      track.title,
+                      overflow: TextOverflow.ellipsis,
+                      style: context.textTheme.titleSmall?.copyWith(
+                        color: context.colorScheme.onBackground,
+                      ),
+                    ),
+                    const SizedBox(height: 3),
+                    Text(
+                      track.artistsNames,
+                      overflow: TextOverflow.ellipsis,
+                      style: context.textTheme.bodySmall?.copyWith(
+                        color: context.colorScheme.onPrimaryContainer,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ListeningHistoryInfoPart extends StatelessWidget {
+  const _ListeningHistoryInfoPart(this.trackListeningHistory);
+
+  final BaseTrackListeningHistory trackListeningHistory;
+
+  @override
+  Widget build(BuildContext context) {
+    final divider = VerticalDivider(
+      color: context.colorScheme.onBackground.withOpacity(.05),
+      thickness: 1.1,
+    );
+    return Tooltip(
+      message:
+          ''' Completed listens count: ${trackListeningHistory.completedListensCount ?? 0}
+      \ntotal uncompleted listens duration: ${trackListeningHistory.uncompletedListensTotalDuration?.formatInHhMmSs} 
+      \n Total listening duration for this day(in minutes): ${trackListeningHistory.totalListeningDuration?.inMinutes}
+      ''',
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // total listens duration in minutes //
+              Expanded(
+                flex: 0,
+                child: _NumberCircle(
+                  title: "Total minutes:  ",
+                  number:
+                      trackListeningHistory.totalListeningDuration?.inMinutes,
+                ),
+              ),
+              Flexible(child: divider),
+              if (constraints.maxWidth > 320) ...[
+                // complete listens count
+                Expanded(
+                  flex: 0,
+                  child: _NumberCircle(
+                    title: "Completed listens count:  ",
+                    number: trackListeningHistory.completedListensCount,
+                  ),
+                ),
+                divider,
+              ],
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class _MoreButton extends StatelessWidget {
+  const _MoreButton({required this.selected, required this.popupMenu});
+
+  final bool selected;
+  final TrackCardPopupMenu popupMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      message: "options",
+      child: InkWell(
+        hoverColor: Colors.transparent,
+        onTapDown: (tapDetails) {
+          showTrackCardPopupMenu(
+            context,
+            selected,
+            Offset(
+              tapDetails.globalPosition.dx,
+              tapDetails.localPosition.dy,
+            ),
+            popupMenu,
+          );
+        },
+        child: Center(
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert_outlined, size: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NumberCircle extends StatelessWidget {
+  const _NumberCircle({required this.number, required this.title});
+
+  final int? number;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          title,
+          style: context.textTheme.bodySmall?.copyWith(
+            color: context.colorScheme.onBackground.withOpacity(.95),
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Container(
+          width: 40,
+          height: 40,
+          decoration: ShapeDecoration(
+            shape: CircleBorder(
+              side: BorderSide(
+                color: context.colorScheme.primaryContainer,
+                width: 1.5,
+              ),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              '${number ?? 0}',
+              style: context.textTheme.titleSmall?.copyWith(
+                color: context.colorScheme.primary,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
