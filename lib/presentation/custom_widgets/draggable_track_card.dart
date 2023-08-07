ae458a5eb2360693d895b8cd394e721a0a995330
DMouayad <muayad.perun@outlook,com>
2023-08-07 14:41:07 +0300

    fix: disable `DraggableTrackCard` on non-desktop platforms

--- lib/presentation/custom_widgets/draggable_track_card.dart
+++ lib/presentation/custom_widgets/draggable_track_card.dart
@@ -1,3 +1,5 @@
+import 'package:dune/presentation/custom_widgets/optional_parent_widget.dart';
+import 'package:dune/support/extensions/context_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fluent;
@@ -18,11 +20,17 @@ class DraggableTrackCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    return fluent.Draggable(
-      feedback: Text("HELLo"),
-      onDragCompleted: () {},
-      feedbackOffset: const fluent.Offset(140, -40),
-      child: child,
+    return OptionalParentWidget(
+      condition: context.isDesktopPlatform,
+      childWidget: child,
+      parentWidgetBuilder: (Widget child) {
+        return fluent.Draggable(
+          feedback: Text("HELLo"),
+          // onDragCompleted: () {},
+          feedbackOffset: const fluent.Offset(140, -40),
+          child: child,
+        );
+      },
     );
   }
 }
