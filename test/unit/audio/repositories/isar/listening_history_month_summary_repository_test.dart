747997bf01af04e4e6ac9281d8b738fec5024aad
DMouayad <muayad.perun@outlook,com>
2023-08-24 12:22:03 +0300

    refactor(tests): create `IsarTestingUtils`

--- test/unit/audio/repositories/isar/listening_history_month_summary_repository_test.dart
+++ test/unit/audio/repositories/isar/listening_history_month_summary_repository_test.dart
@@ -1,20 +1,21 @@
 import 'package:dune/data/audio/isar/models/isar_track_listening_history.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../utils/isar_test_db.dart';
+import '../../../../utils/isar_testing_utils.dart';
 
 DateTime get date => DateTime.now();
 
 void main() {
-  setUpAll(() async => await initIsarForTesting());
-  setUp(() async => await refreshDatabase());
+  setUpAll(() async => await IsarTestingUtils.initIsarForTesting());
+  setUp(() async => await IsarTestingUtils.refreshDatabase());
   test(
     'it should return null when no tracks listening history exists for month',
     () async {
       final noTrackListeningHistoriesExists =
-          (await isar.isarTrackListeningHistorys.count()) == 0;
+          (await IsarTestingUtils.isar.isarTrackListeningHistorys.count()) == 0;
       expectLater(noTrackListeningHistoriesExists, true);
-      final result = await isarMusicRepo.listeningHistory.getMonthSummary(
+      final result =
+          await IsarTestingUtils.isarMusicRepo.listeningHistory.getMonthSummary(
         month: date.month,
         year: date.year,
       );
@@ -26,13 +27,15 @@ void main() {
     "it should create a new [BaseListeningHistoryMonthSummary] when it's not found"
     " in the db and track listening histories in the month",
     () async {
-      final monthSummaryBefore = (await isarMusicRepo.listeningHistory
+      final monthSummaryBefore = (await IsarTestingUtils
+              .isarMusicRepo.listeningHistory
               .getMonthSummary(month: date.month, year: date.year))
           .requireValue;
       // assert no month summary exists for [date.month]
       expectLater(monthSummaryBefore, null);
-      await isarTrackListeningHistorySeeder.seedCount();
-      final monthSummary = (await isarMusicRepo.listeningHistory
+      await IsarTestingUtils.trackListeningHistorySeeder.seedRandomCount();
+      final monthSummary = (await IsarTestingUtils
+              .isarMusicRepo.listeningHistory
               .getMonthSummary(month: date.month, year: date.year))
           .requireValue;
       expectLater(monthSummary != null, true);
---
+++ test/utils/isar_testing_utils.dart
@@ -0,0 +1,93 @@
+import 'package:dune/data/audio/isar/repositories/isar_music_repository.dart';
+import 'package:dune/data/audio/isar/seeders/isar_track_listening_history_seeder.dart';
+import 'package:dune/domain/audio/base_models/base_album.dart';
+import 'package:dune/domain/audio/base_models/base_artist.dart';
+import 'package:dune/domain/audio/base_models/base_playlist.dart';
+import 'package:dune/domain/audio/base_models/base_track.dart';
+import 'package:dune/domain/audio/factories/album_factory.dart';
+import 'package:dune/domain/audio/factories/artist_factory.dart';
+import 'package:dune/domain/audio/factories/playlist_factory.dart';
+import 'package:dune/domain/audio/factories/track_factory.dart';
+import 'package:dune/domain/audio/seeders/album_seeder.dart';
+import 'package:dune/domain/audio/seeders/artist_seeder.dart';
+import 'package:dune/domain/audio/seeders/playlist_seeder.dart';
+import 'package:dune/domain/audio/seeders/track_seeder.dart';
+import 'package:dune/support/enums/music_source.dart';
+import 'package:dune/support/helpers/isar_helper.dart';
+import 'package:get_it/get_it.dart';
+import 'package:isar/isar.dart';
+
+class IsarTestingUtils {
+  static late Isar _isar;
+
+  static Isar get isar => _isar;
+  static late IsarMusicRepository _isarMusicRepo;
+
+  static IsarMusicRepository get isarMusicRepo => _isarMusicRepo;
+
+  static Future<void> initIsarForTesting() async {
+    final isarDBNotOpened = !GetIt.instance.isRegistered<Isar>();
+    if (isarDBNotOpened) {
+      _isar = await IsarHelper.initForTesting();
+      GetIt.instance.registerSingleton<Isar>(_isar);
+    } else {
+      _isar = GetIt.instance.get();
+    }
+    _isarMusicRepo = IsarMusicRepository(isar: _isar);
+  }
+
+  static Future<void> refreshDatabase() async {
+    _isarMusicRepo = IsarMusicRepository(isar: _isar);
+    await _isar.writeTxn(() async => await _isar.clear());
+  }
+
+  static IsarTrackListeningHistorySeeder get trackListeningHistorySeeder {
+    return IsarTrackListeningHistorySeeder(_isarMusicRepo);
+  }
+
+  static Future<List<BaseTrack>> seedTracks(
+    int count,
+    MusicSource source,
+  ) async {
+    return await TrackSeeder(_isarMusicRepo.tracks).seedCount(
+      count,
+      TrackFactory().setMusicSource(source),
+    );
+  }
+
+  static Future<List<BasePlaylist>> seedPlaylists(
+    int count, [
+    MusicSource? playlistSource,
+    int playlistTracksCount = 1,
+  ]) async {
+    return await PlaylistSeeder(_isarMusicRepo.playlists).seedCount(
+      count,
+      factory: PlaylistFactory()
+          .setTracksCount(playlistTracksCount)
+          .setMusicSource(playlistSource),
+    );
+  }
+
+  static Future<List<BaseAlbum>> seedAlbums(
+    int count, [
+    MusicSource? albumMusicSource,
+    int? albumTracksCount,
+  ]) async {
+    return await AlbumSeeder(_isarMusicRepo.albums).seedCount(
+      count,
+      AlbumFactory()
+          .setTracksCount(albumTracksCount)
+          .setMusicSource(albumMusicSource),
+    );
+  }
+
+  static Future<List<BaseArtist>> seedArtists(
+    int count, [
+    MusicSource? artistMusicSource,
+  ]) async {
+    return await ArtistSeeder(_isarMusicRepo.artists).seedCount(
+      count,
+      ArtistFactory().setMusicSource(artistMusicSource),
+    );
+  }
+}
