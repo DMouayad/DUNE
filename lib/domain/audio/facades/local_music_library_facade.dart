90cac5d3f96c14b09516561c8d333574063b9618
DMouayad <muayad.perun@outlook,com>
2023-08-25 15:05:26 +0300

    refactor(LocalMusicLibraryFacade): add a helper function for cached `MusicLibrary`

--- lib/domain/audio/facades/local_music_library_facade.dart
+++ lib/domain/audio/facades/local_music_library_facade.dart
@@ -14,8 +14,8 @@ class LocalMusicLibraryFacade {
   MusicLibrary _cachedLibrary = MusicLibrary();
 
   /// Loads a `MusicLibrary` from local storage.
-  /// the [queryOptions] will be used to retrieve this library media(tracks, artists,
-  /// albums, playlists).
+  /// the [queryOptions] will be used to retrieve this library
+  /// media: tracks, artists, albums, playlists.
   FutureOrResult<MusicLibrary> getLibrary([
     QueryOptions queryOptions = QueryOptions.defaultOptions,
   ]) async {
@@ -44,6 +44,8 @@ class LocalMusicLibraryFacade {
 
   MusicSource get _localSource => MusicSource.local;
 
+  /// Saves the given [tracks] in local storage then returns
+  /// a `MusicLibrary` with the new tracks added.
   FutureResult<MusicLibrary> addTracksToLibrary(
     List<BaseTrack> tracks,
   ) async {
@@ -55,26 +57,46 @@ class LocalMusicLibraryFacade {
   }
 
   FutureOrResult<List<BaseArtist>> getArtists(QueryOptions queryOptions) async {
-    return _cachedLibrary.getArtists(queryOptions)?.asResult ??
-        (await _artistRepository.findAllWhereSource(_localSource, queryOptions))
-            .fold(
-                onSuccess: (value) =>
-                    _cachedLibrary.setArtists(value, queryOptions));
+    return await _getFromCachedLibrary(
+      _cachedLibrary.getArtists(queryOptions),
+      ifAbsent: () async => await _artistRepository.findAllWhereSource(
+          _localSource, queryOptions),
+      onIfAbsentSuccess: (value) =>
+          _cachedLibrary.setArtists(value, queryOptions),
+    );
   }
 
   FutureOrResult<List<BaseTrack>> getTracks(QueryOptions queryOptions) async {
-    return _cachedLibrary.getTracks(queryOptions)?.asResult ??
-        (await _trackRepository.findAllWhereSource(_localSource, queryOptions))
-            .fold(onSuccess: (value) {
-          _cachedLibrary.setTracks(value, queryOptions);
-        });
+    return await _getFromCachedLibrary(
+      _cachedLibrary.getTracks(queryOptions),
+      ifAbsent: () async =>
+          await _trackRepository.findAllWhereSource(_localSource, queryOptions),
+      onIfAbsentSuccess: (value) =>
+          _cachedLibrary.setTracks(value, queryOptions),
+    );
   }
 
   FutureOrResult<List<BaseAlbum>> getAlbums(QueryOptions queryOptions) async {
-    return _cachedLibrary.getAlbums(queryOptions)?.asResult ??
-        (await _albumRepository.findAllWhereSource(_localSource, queryOptions))
-            .fold(
-          onSuccess: (value) => _cachedLibrary.setAlbums(value, queryOptions),
-        );
+    return await _getFromCachedLibrary(
+      _cachedLibrary.getAlbums(queryOptions),
+      ifAbsent: () async =>
+          await _albumRepository.findAllWhereSource(_localSource, queryOptions),
+      onIfAbsentSuccess: (value) =>
+          _cachedLibrary.setAlbums(value, queryOptions),
+    );
+  }
+
+  /// a helper function
+  ///
+  /// if [cachedItems] is empty, it calls [ifAbsent] to get the items from
+  /// local storage.
+  /// then if [ifAbsent] is a success, calls [onIfAbsentSuccess]
+  FutureOrResult<T> _getFromCachedLibrary<T extends List>(
+    T cachedItems, {
+    required FutureOrResult<T> Function() ifAbsent,
+    required void Function(T value) onIfAbsentSuccess,
+  }) async {
+    if (cachedItems.isNotEmpty) return cachedItems.asResult;
+    return (await ifAbsent()).fold(onSuccess: onIfAbsentSuccess);
   }
 }
