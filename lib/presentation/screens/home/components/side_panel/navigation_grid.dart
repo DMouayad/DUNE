6c927f833cdb00468ed0e2b37d4826a44c4c0c6e
DMouayad <muayad.perun@outlook,com>
2023-09-20 11:43:02 +0300

    feat(NavGrid): hide buttons when panel is closed and `TabsMode.isVertical`

--- lib/presentation/screens/home/components/side_panel/navigation_grid.dart
+++ lib/presentation/screens/home/components/side_panel/navigation_grid.dart
@@ -1,5 +1,6 @@
 import 'package:dune/navigation/app_router.dart';
 import 'package:dune/presentation/providers/shared_providers.dart';
+import 'package:dune/presentation/providers/state_controllers.dart';
 import 'package:dune/support/extensions/context_extensions.dart';
 import 'package:dune/support/themes/theme_constants.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,9 +10,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'side_panel.dart';
 
-const _kButtonWidth = 52.0;
+const _kButtonWidth = 50.0;
 
-class NavGrid extends StatelessWidget {
+class NavGrid extends ConsumerWidget {
   const NavGrid({
     super.key,
     required this.extended,
@@ -24,47 +25,56 @@ class NavGrid extends StatelessWidget {
   final double width;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final isVerticalTabsMode =
+        ref.watch(appPreferencesController).tabsMode.isVertical;
+    final showAllButtons = (isVerticalTabsMode && extended) || extended;
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 12),
       alignment: Alignment.center,
-      width: extended ? width : _kButtonWidth,
       child: Wrap(
         spacing: 6,
         runSpacing: 10,
         children: [
-          Consumer(builder: (context, ref, _) {
-            return _NavButton(
-              iconData: CupertinoIcons.sidebar_left,
-              backgroundColor: Colors.transparent,
-              iconColor: extended
-                  ? context.colorScheme.primary
-                  : context.colorScheme.onPrimaryContainer,
-              onPressed: () {
-                if (extended) {
-                  ref.read(navigationRailSizeProvider.notifier).state =
-                      kSidePanelMinWidth;
-                } else {
-                  ref.read(navigationRailSizeProvider.notifier).state =
-                      context.maxNavRailWidth;
-                }
-              },
-            );
-          }),
           _NavButton(
-            iconData: fluent.FluentIcons.settings,
-            onPressed: () =>
-                onDestinationSelected(QuickNavDestination.settingsPage),
+            iconData: CupertinoIcons.sidebar_left,
+            backgroundColor: Colors.transparent,
+            iconColor: extended
+                ? context.colorScheme.primary
+                : context.colorScheme.onPrimaryContainer,
+            onPressed: () {
+              if (extended) {
+                ref.read(navigationRailSizeProvider.notifier).state =
+                    kSidePanelMinWidth;
+              } else {
+                ref.read(navigationRailSizeProvider.notifier).state =
+                    context.maxNavRailWidth;
+              }
+            },
           ),
-          _NavButton(
-            iconData: CupertinoIcons.rectangle_stack_badge_person_crop,
-            onPressed: () =>
-                onDestinationSelected(QuickNavDestination.listeningHistoryPage),
+          Visibility(
+            visible: showAllButtons,
+            child: _NavButton(
+              iconData: fluent.FluentIcons.settings,
+              onPressed: () =>
+                  onDestinationSelected(QuickNavDestination.settingsPage),
+            ),
           ),
-          _NavButton(
-            iconData: CupertinoIcons.compass,
-            onPressed: () =>
-                onDestinationSelected(QuickNavDestination.explorePage),
+          Visibility(
+            visible: showAllButtons,
+            child: _NavButton(
+              iconData: CupertinoIcons.rectangle_stack_badge_person_crop,
+              onPressed: () => onDestinationSelected(
+                  QuickNavDestination.listeningHistoryPage),
+            ),
+          ),
+          Visibility(
+            visible: showAllButtons,
+            child: _NavButton(
+              iconData: CupertinoIcons.compass,
+              onPressed: () =>
+                  onDestinationSelected(QuickNavDestination.explorePage),
+            ),
           ),
         ],
       ),
@@ -92,6 +102,7 @@ class _NavButton extends StatelessWidget {
         shape: MaterialStateProperty.all(
             const RoundedRectangleBorder(borderRadius: kBorderRadius)),
         fixedSize: MaterialStateProperty.all(const Size.square(_kButtonWidth)),
+        padding: MaterialStateProperty.all(const EdgeInsets.all(4)),
         backgroundColor: MaterialStateProperty.resolveWith((states) {
           if (states.contains(MaterialState.hovered)) {
             return context.colorScheme.onInverseSurface;
