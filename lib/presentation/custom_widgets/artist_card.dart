d781f526a5a011f7b48faab44ecb80150cda879a
DMouayad <muayad.perun@outlook,com>
2023-09-26 13:01:17 +0300

    refactor: create `ArtistCard`

---
+++ lib/presentation/custom_widgets/artist_card.dart
@@ -0,0 +1,58 @@
+import 'package:dune/domain/audio/base_models/base_artist.dart';
+import 'package:dune/presentation/controllers/selection_controller.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_card.dart';
+import 'selectable_card.dart';
+
+class ArtistCard extends StatelessWidget {
+  const ArtistCard({super.key, required this.artist, this.width});
+
+  final BaseArtist artist;
+  final double? width;
+
+  const factory ArtistCard.selectable({
+    required BaseArtist artist,
+    required SelectionState<BaseArtist> selectionState,
+    required void Function() onSelected,
+    double? width,
+  }) = _SelectableArtistCard;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+      width: width,
+      tag: artist.id ?? artist.browseId ?? artist.hashCode.toString(),
+      title: artist.name,
+      thumbnails: artist.thumbnails,
+      shape: BoxShape.circle,
+      onTap: () {},
+    );
+  }
+}
+
+class _SelectableArtistCard extends ArtistCard {
+  final SelectionState<BaseArtist> selectionState;
+  final void Function() onSelected;
+
+  const _SelectableArtistCard({
+    required super.artist,
+    required this.selectionState,
+    required this.onSelected,
+    super.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final selectionKey = artist.id ?? artist.hashCode.toString();
+
+    return SelectableCard<BaseArtist>(
+      selectionKey: selectionKey,
+      popupMenu: const SizedBox(),
+      onTap: () {},
+      selectionState: selectionState,
+      onSelected: onSelected,
+      child: ArtistCard(artist: artist, width: width),
+    );
+  }
+}
