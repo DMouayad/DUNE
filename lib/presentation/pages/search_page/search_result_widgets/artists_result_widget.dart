49716e194f954089a4084369c6209ea71984389a
DMouayad <muayad.perun@outlook,com>
2023-09-26 13:01:17 +0300

    refactor: create `ArtistCard`

--- lib/presentation/pages/search_page/search_result_widgets/artists_result_widget.dart
+++ lib/presentation/pages/search_page/search_result_widgets/artists_result_widget.dart
@@ -1,11 +1,11 @@
 import 'package:dune/domain/audio/base_models/base_artist.dart';
+import 'package:dune/presentation/custom_widgets/artist_card.dart';
 import 'package:dune/presentation/custom_widgets/shimmer_widget.dart';
 
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'search_result_items_grid.dart';
-import '../../../custom_widgets/custom_card.dart';
 
 const maxCardHeight = 180.0;
 
@@ -23,15 +23,7 @@ class ArtistsSearchResults extends ConsumerWidget {
           error: (err, stack) => Text('error $err'),
           loading: () => const _ArtistsShimmer(),
           data: (artists) {
-            final artist = artists.elementAt(index);
-            return CustomCard(
-              width: maxCardHeight,
-              tag: artist.id ?? artist.browseId ?? artist.hashCode.toString(),
-              title: artist.name,
-              thumbnails: artist.thumbnails,
-              shape: BoxShape.circle,
-              onTap: () {},
-            );
+            return ArtistCard(artist: artists.elementAt(index));
           },
         );
       },
